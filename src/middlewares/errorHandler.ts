3277aed708752cfc8e560a697524384275efc025
Ivan <karachun04@gmail.com>
2025-07-31 12:17:00 +0300

    fix: Changed function argument name to get rid of eslint warning.

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -10,7 +10,7 @@ const sendError = (err: CustomError, res: Response) => {
   });
 };
 
-const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err: CustomError, req: Request, res: Response, _next: NextFunction) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
