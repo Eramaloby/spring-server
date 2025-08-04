f9e337881b95ff3ae024cacb9282b80a9749b30c
Ivan <karachun04@gmail.com>
2025-08-04 21:36:33 +0300

    fix: Removed try/catch blocks around synchronous code.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,27 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { LoginSuccessResponse, LoginRequestBody, LoginErrorResponse } from '../types/auth.types';
 import { authService } from '../services/authService';
-import AppError from '../utils/AppError';
 
 export const authUser = (
   req: Request<object, object, LoginRequestBody>,
-  res: Response<LoginSuccessResponse | LoginErrorResponse>,
-  next: NextFunction
+  res: Response<LoginSuccessResponse | LoginErrorResponse>
 ) => {
   const { login, password } = req.body;
 
-  let result;
-  try {
-    result = authService.authenticate(login, password);
-  } catch (error) {
-    if (error instanceof AppError) {
-      next(error);
-    } else {
-      next(new AppError('An unexpected error occurred.', 500));
-    }
-  }
+  const result = authService.authenticate(login, password);
 
-  if (result) {
-    return res.status(200).json(result);
-  }
+  return res.status(200).json(result);
 };
--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,14 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 
 import { type Product, PRODUCTS_BlOCK_CONTENT } from '../constants/productsBlockContent';
 import { productService } from '../services/productsService';
 
-import AppError from '../utils/AppError';
-
 export const getProducts = (
   req: Request<object, object, object, { search?: string }>,
-  res: Response<Product[] | { message: string }>,
-  next: NextFunction
+  res: Response<Product[] | { message: string }>
 ) => {
   const search = req.query.search;
 
@@ -16,10 +13,6 @@ export const getProducts = (
 
   if (search) {
     resultProducts = productService.searchProducts(search);
-  }
-
-  if (resultProducts.length === 0) {
-    return next(new AppError('No products matching search terms', 404));
   }
 
   return res.status(200).json(resultProducts);
