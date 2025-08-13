0ed5f064a7a865f6eed8bfaf3783875b1a2703f0
Ivan <karachun04@gmail.com>
2025-08-13 18:56:16 +0300

    feat: Now server trows 401 error when refresh token expires.

--- src/middlewares/accessTokenValidator.ts
+++ src/middlewares/accessTokenValidator.ts
@@ -6,13 +6,13 @@ const accessTokenValidator = (req: Request, _res: Response, next: NextFunction) 
   const authHeader = req.headers['authorization'];
   const accessToken = authHeader && authHeader.split(' ')[1];
 
-  if (!accessToken) {
-    return next(new AppError('Access token not found.', 401));
-  }
   try {
+    if (!accessToken) {
+      throw new AppError('Access token not found', 401);
+    }
     tokenService.validateAccessToken(accessToken);
-  } catch (error) {
-    next(error);
+  } catch {
+    next(new AppError('Unauthorized', 401));
   }
 
   next();
